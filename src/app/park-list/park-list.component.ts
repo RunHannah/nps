10a3d7585adf48aefb745356d6962183b7ab5bdb
Guest User <Guest@Epicodus-4A.local>
2018-03-30 11:56:51 -0700

    working on filter pipe.

--- src/app/park-list/park-list.component.ts
+++ src/app/park-list/park-list.component.ts
@@ -7,29 +7,10 @@ import { Park } from '../models/park.model';
   styleUrls: ['./park-list.component.css']
 })
 export class ParkListComponent {
+  filterByState: string = "byState";
 
-  constructor() { }
-
-
-
-  /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-  myFunction() {
-      document.getElementById("myDropdown").classList.toggle("show");
+  onChange(optionFromMenu) {
+    this.filterByState = optionFrom Menu;
   }
 
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
 }

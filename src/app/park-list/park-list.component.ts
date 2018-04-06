96c8c972976d4a2efa996597583c0e54bfaa8e0a
Guest User <Guest@Epicodus-3A.local>
2018-04-06 14:19:23 -0700

    OMG api call works.

--- src/app/park-list/park-list.component.ts
+++ src/app/park-list/park-list.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Park } from '../models/park.model';
+import { Component, Input } from '@angular/core';
+// import { Park } from '../models/park.model';
 
 @Component({
   selector: 'app-park-list',
   templateUrl: './park-list.component.html',
-  styleUrls: ['./park-list.component.css']
+  styleUrls: ['./park-list.component.css'],
+  providers: []
 })
 export class ParkListComponent {
-  filterByState: string = "byState";
+  // filterByState: string = "byState";
+  @Input() childParks;
+  constructor() {}
 
-  onChange(optionFromMenu) {
-    this.filterByState = optionFromMenu;
-  }
+  // onChange(optionFromMenu) {
+  //   this.filterByState = optionFromMenu;
+  // }
+
+
 
 }

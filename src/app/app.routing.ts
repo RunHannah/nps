1278d326fc3312d58296e8804bf4445dff8eb8b6
Guest User <Guest@Epicodus-3A.local>
2018-04-06 13:54:50 -0700

    change use of park-list component to display data from api call.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { BlogPostsComponent } from './blog-posts/blog-posts.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { StateFormComponent } from './state-form/state-form.component';
 
 const appRoutes: Routes = [
   {
@@ -14,6 +15,10 @@ const appRoutes: Routes = [
   {
     path: 'blog-details/:id',
     component: BlogDetailComponent
+  },
+  {
+    path: 'results',
+    component: StateFormComponent
   }
 ];
 
--- src/app/state-form/state-form.component.ts
+++ src/app/state-form/state-form.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { NpsApiService } from '../nps-api.service';
 
 @Component({
   selector: 'app-state-form',
   templateUrl: './state-form.component.html',
-  styleUrls: ['./state-form.component.css']
+  styleUrls: ['./state-form.component.css'],
+  providers: [ NpsApiService ]
 })
-export class StateFormComponent implements OnInit {
+export class StateFormComponent {
+  parksResult: any[]=null;
+  constructor(private listOfParks: NpsApiService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  getParks(state: string) {
+    this.listOfParks.getByState(state).subscribe(response => {
+      this.parksResult = response.json();
+    });
   }
 
 }

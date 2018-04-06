b46764b8dc8bc89a2ce6b732e60186c52ee8ab2f
Hannah Lee <hanlavlee@gmail.com>
2018-04-05 22:35:45 -0700

    update blog-posts.json file, install and configure to firebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FilterPipe } from './find-park.pipe';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -10,6 +12,21 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { BlogPostsComponent } from './blog-posts/blog-posts.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+// import Blog Details Component...
+
+// import { routing } from './app.routing';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -22,7 +39,13 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    // routing,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/blog-posts/blog-posts.component.ts
+++ src/app/blog-posts/blog-posts.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Blog } from '../models/blog.model';
+// import { BlogService } from '../blog.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-posts',
   templateUrl: './blog-posts.component.html',
-  styleUrls: ['./blog-posts.component.css']
+  styleUrls: ['./blog-posts.component.css'],
+  providers: [BlogService]
 })
 export class BlogPostsComponent implements OnInit {
+  blogs: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private blogService: BlogService) { }
 
   ngOnInit() {
+    this.blogs = this.blogService.getBlogs();
   }
+
+  goToDetailPage(clickedBlog) {
+    this.router.navigate(['blog-details', clickedBlog.$key]);
+  };
 
 }

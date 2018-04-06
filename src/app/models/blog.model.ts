532351fa592b092ddc94c6c49d69f451f10486e4
Hannah Lee <hanlavlee@gmail.com>
2018-04-05 20:50:22 -0700

    create blog.model.ts, blog-posts.json file and being inputting key value data.

---
+++ src/app/models/blog.model.ts
@@ -0,0 +1,10 @@
+export class Blog {
+  constructor (
+    public featureTitle: string,
+    public featureDescription: string,
+    public pageTitle: string,
+    public pageText: string,
+    public imageUrl: string,
+    public imageText: string,
+  ) {}
+}
